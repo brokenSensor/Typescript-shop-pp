6f6f60d98c4e33c36f82098f312e47c837c4b82a
brokenSensor <jonimen95@yandex.ru>
2021-07-12 20:47:09 +0800

    Product, loader, rating components and main screen

--- frontend/src/screens/MainScreen.tsx
+++ frontend/src/screens/MainScreen.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Col, Row } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
 import { useGetAllProductsQuery } from '../api/productApi'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import Product from '../components/Product'
 
 const MainScreen: React.FC = () => {
 	const { isLoading, data, error } = useGetAllProductsQuery()
@@ -13,16 +15,23 @@ const MainScreen: React.FC = () => {
 				<h1>{error}</h1>
 			) : (
 				<>
-					<Row>
-						{data &&
-							data.map(product => (
-								<Col key={product.id} sm={12} md={6} lg={4} xl={3}>
-									<Link to={`/product/${product.id}`}>
-										<h3>{product.name}</h3>
-									</Link>
-								</Col>
-							))}
-					</Row>
+					<h1>Latest Products</h1>
+					{isLoading ? (
+						<Loader />
+					) : error ? (
+						<Message variant='danger'>{error}</Message>
+					) : (
+						<>
+							<Row>
+								{data &&
+									data.map(product => (
+										<Col key={product.id} sm={12} md={6} lg={4} xl={3}>
+											<Product product={product} />
+										</Col>
+									))}
+							</Row>
+						</>
+					)}
 				</>
 			)}
 		</>
