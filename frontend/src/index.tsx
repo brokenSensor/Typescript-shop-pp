5bcc51ba86c24e1556e884a3399fedd0f94d9743
brokenSensor <jonimen95@yandex.ru>
2021-09-08 14:57:31 +0800

    Bootswatch fix

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'bootswatch/dist/quartz/bootstrap.min.css'
 import './index.sass'
 import App from './App'
 import { Provider } from 'react-redux'
